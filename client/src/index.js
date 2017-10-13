8213b7600f122f620001a9015a381d5d7ac54cba
Jackie Liu <ljmlionshy@live.com>
2017-10-13 09:47:40 +0800

    use gitkraken to make commit

--- client/src/index.js
+++ client/src/index.js
@@ -9,7 +9,8 @@ import sagas from 'sagas';
 
 const store = configureStore();
 store.runSaga(sagas);
-ReactDOM.render(<Provider store={store}>
-<App />
+ReactDOM.render(
+<Provider store={store}>
+    <App/>
 </Provider>, document.getElementById('root'));
 registerServiceWorker();

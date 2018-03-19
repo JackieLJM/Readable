4b444f95c67c9e2bc36230ff51a4c634657a8cd7
JackieLJM <ljmlionshy@live.com>
2018-03-19 17:14:16 +0800

    readable api

--- app/src/utils/api.js
+++ app/src/utils/api.js
@@ -6,18 +6,21 @@ if (!token) {
 const header = {
     "Accpet": "application/json",
     "Authorization": token,
+    "content-type":"application/json"
 }
 export const getAllCatagories = () => { return fetch().then() };
 export const getPostsByCatagories = () => { return fetch().then() };
+
 export const getAllPosts = () => { return fetch().then() };
-export const addPost = () => { return fetch().then() };
-export const getPostById = () => { return fetch().then() };
+export const addAPost = () => { return fetch().then() };
+export const getAPostById = () => { return fetch().then() };
 export const delPostById = () => { return fetch().then() };
-export const votePostById = () => { return fetch().then() };
+export const voteAPostById = () => { return fetch().then() };
 export const updatePostById = () => { return fetch().then() };
+
 export const getCommentsByPostId = () => { return fetch().then() };
 export const getCommentsById = () => { return fetch().then() };
 export const updateCommentsById = () => { return fetch().then() };
 export const addComments = () => { return fetch().then() };
 export const voteCommentsBy = () => { return fetch().then() };
-export const = () => { return fetch().then() };
+export const deleteCommentsById= () => { return fetch().then() };
